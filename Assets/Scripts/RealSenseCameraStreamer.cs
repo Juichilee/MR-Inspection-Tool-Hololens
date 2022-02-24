6150c8f7d11a9a2232915c774442a3d6bae56296
Juichilee <leejuic@oregonstate.edu>
2022-02-23 16:14:14 -0800

    Update RealSenseCameraStreamer.cs

--- Assets/Scripts/RealSenseCameraStreamer.cs
+++ Assets/Scripts/RealSenseCameraStreamer.cs
@@ -11,52 +11,52 @@ public class RealSenseCameraStreamer : MonoBehaviour
     public Renderer rend;
     public Material rgbMat;
 
-    [Range(0.0f, 1.0f)]
-    public float thresholdVal = 0.5f;
-
-    [Range(0.0f, 1.0f)]
-    public float focalVal = 0.5f;
-    
-    public void rsTFsetMax(SliderEventData eventData) 
+    [Range(0, 1)]
+    [SerializeField]
+    private float thresholdVal = 0.5f;
+    public float ThresholdVal
     {
-        //rsTF.SetMaxDistance(eventData.NewValue * 4);
+        get { return thresholdVal; }
+        set
+        {
+            thresholdVal = value;
+            OnUpdate();
+        }
+    }
+
+    [Range(0, 1)]
+    [SerializeField]
+    private float focalVal = 0.5f;
+    public float FocalVal
+    {
+        get { return focalVal; }
+        set
+        {
+            focalVal = value;
+            OnUpdate();
+        }
+    }
+
+    public void OnUpdate()
+    {
         rsTF.SetMaxDistance(thresholdVal * 4);
-
-    }
-
-    public void canvasSetFocal(SliderEventData eventData)
-    {
-        //canvas.planeDistance = eventData.NewValue * 3 + 0.5f;
         canvas.planeDistance = focalVal * 3 + 0.5f;
-
     }
 
-    private void Awake()
+    public void setThresholdVal(SliderEventData eventData)
     {
-        
+        ThresholdVal = eventData.NewValue;
     }
-
-
-    private void Start()
+    public void setFocalVal(SliderEventData eventData)
     {
-        //WebCamDevice[] devices = WebCamTexture.devices;
-        //// for debugging purposes, prints available devices to the console
-        //for (int i = 0; i < devices.Length; i++)
-        //{
-        //    print("Webcam available: " + devices[i].name);
-        //}
-
-        //// assuming the first available WebCam is desired
-        //WebCamTexture tex = new WebCamTexture(devices[0].name);
-        //rend.material.mainTexture = tex;
-        
-        //tex.Play();
+        FocalVal = eventData.NewValue;
     }
 
     private void Update()
     {
-        rsTF.SetMaxDistance(thresholdVal * 4);
-        canvas.planeDistance = focalVal * 3 + 0.5f;
+        // Debug Purpose
+        ThresholdVal = thresholdVal;
+        FocalVal = focalVal;
         rend.material.mainTexture = rgbMat.mainTexture;
     }
 }
