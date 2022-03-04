d5603d4124481451f39df663ddd1ceb0425eab3f
Juichilee <leejuic@oregonstate.edu>
2022-03-03 16:42:12 -0800

    Soft Reset

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,25 +7,55 @@ using Unity.RenderStreaming;
 public class InputManager : MonoBehaviour
 {
     public static InputManager _InputManagerInstance;
-    private VirtualCameraStreamer VirtualCameraStreamer;
-    private RealSenseCameraStreamer RealCameraStreamer;
+    //private VirtualCameraStreamer VirtualCameraStreamer;
+    //private RealSenseCameraStreamer RealCameraStreamer;
 
-    public bool objectRecognitionModeOn = false;
-    public bool virtualObjectRecognitionModeOn = false;
-    public bool realSensePointCloudOverlayOn = false;
+    //public bool objectRecognitionModeOn = false;
+    //public bool virtualObjectRecognitionModeOn = false;
+    //public bool realSensePointCloudOverlayOn = false;
 
     public Vector2Int streamingSize = new Vector2Int(512, 512);
-    public float depthThreshold;
+    public Material inputMat;
+    public Renderer image;
+    //public float depthThreshold;
 
     private VideoStreamTrack track;
 
-    private RenderTexture finalRenderTexture;
+    //private RenderTexture finalRenderTexture;
+
+    bool trackAdded = false;
+
+    public static RenderTextureFormat GetSupportedRenderTextureFormat(UnityEngine.Rendering.GraphicsDeviceType type)
+    {
+        switch (type)
+        {
+            case UnityEngine.Rendering.GraphicsDeviceType.Direct3D11:
+            case UnityEngine.Rendering.GraphicsDeviceType.Direct3D12:
+            case UnityEngine.Rendering.GraphicsDeviceType.Vulkan:
+                return RenderTextureFormat.BGRA32;
+            case UnityEngine.Rendering.GraphicsDeviceType.OpenGLCore:
+            case UnityEngine.Rendering.GraphicsDeviceType.OpenGLES2:
+            case UnityEngine.Rendering.GraphicsDeviceType.OpenGLES3:
+                return RenderTextureFormat.ARGB32;
+            case UnityEngine.Rendering.GraphicsDeviceType.Metal:
+                return RenderTextureFormat.BGRA32;
+        }
+        return RenderTextureFormat.Default;
+    }
 
     private void Awake()  //Used to initialize any variables or gams state before the game starts. Only called once
     {
         _InputManagerInstance = this;
-        VirtualCameraStreamer = this.GetComponent<VirtualCameraStreamer>();
-        RealCameraStreamer = this.GetComponent<RealSenseCameraStreamer>();
+
+        // Get a valid RendertextureFormat
+        var gfxType = SystemInfo.graphicsDeviceType;
+        RenderTextureFormat format = GetSupportedRenderTextureFormat(gfxType);
+        Debug.Log("Format: " + format);
+        RenderTexture rend = new RenderTexture(streamingSize.x, streamingSize.y, 0, UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm);
+        inputMat.mainTexture = rend;
+        image.material = inputMat;
+        //VirtualCameraStreamer = this.GetComponent<VirtualCameraStreamer>();
+        //RealCameraStreamer = this.GetComponent<RealSenseCameraStreamer>();
 
     }
     // Start is called before the first frame update. Used to pass information between referenced scripts
@@ -37,8 +67,13 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        track = new VideoStreamTrack("video", VirtualCameraStreamer.rt);
-        RenderStreaming.Instance.AddVideoStreamTrack(track);
+        if (!trackAdded)
+        {
+            //track = new VideoStreamTrack("video", VirtualCameraStreamer.rt);
+            //RenderStreaming.Instance.AddVideoStreamTrack(track);
+            trackAdded = true;
+        }
+        
     }
 
     void SendRenderTextureToWebRTCServer()
