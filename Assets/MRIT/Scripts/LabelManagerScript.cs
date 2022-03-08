6aa810ccbabbb5a8028c34a29f979fb8cbdf01cd
Juichilee <leejuic@oregonstate.edu>
2022-03-07 18:43:03 -0800

    Demo 1 Commit

--- Assets/MRIT/Scripts/LabelManagerScript.cs
+++ Assets/MRIT/Scripts/LabelManagerScript.cs
@@ -13,6 +13,8 @@ public class LabelManagerScript : MonoBehaviour
     public Transform RSTransform;
     public Camera RSCameraView;
 
+    public Vector2 textureSize;
+
     private void Awake()
     {
         SharedInstance = this;
@@ -21,6 +23,7 @@ public class LabelManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        textureSize = InputManager._InputManagerInstance.streamingSize;
         pooledObjects = new List<GameObject>();
         GameObject tmp;
         for(int i = 0; i < AmountToPool; i++)
@@ -29,6 +32,7 @@ public class LabelManagerScript : MonoBehaviour
             tmp.SetActive(false);
             pooledObjects.Add(tmp);
         }
+        
     }
 
     public GameObject GetPooledObject()
@@ -43,6 +47,8 @@ public class LabelManagerScript : MonoBehaviour
         return null;
     }
 
+    Vector3 raycastPoint;
+
     public void ProcessLabelJSON(string content)
     {
         //LabelInfo frame = JsonUtility.FromJson<LabelInfo>(content);
@@ -50,33 +56,103 @@ public class LabelManagerScript : MonoBehaviour
         string[] pred_classes = frame.pred_classes;
         float[] scores = frame.scores;
         float[][] boxes = frame.boxes;
+
+        Vector2 startingPos = new Vector2(0,0);
+        Vector2 endingPos = new Vector2(0,0);
+        raycastPoint = new Vector3(0, 0, 0);
+
         Debug.Log($"JSON String: {content}");
         
         for(int i = 0; i < pred_classes.Length; i++)
         {
             string currClass = pred_classes[i];
             float[] boxPositions = boxes[i];
+
+            float x = 0.00005f;
+            Debug.Log($"PredClass: {currClass}, box1: {boxPositions[0].GetType()}, box2: {x.GetType()}");
+
+            startingPos.x = boxPositions[0];
+            //Debug.Log("Setting Pos Success1");
+            startingPos.y = InvertYPos(boxPositions[1]);
+            //Debug.Log("Setting Pos Success2");
+            endingPos.x = boxPositions[2];
+            //Debug.Log("Setting Pos Success3");
+            endingPos.y = InvertYPos(boxPositions[3]);
+            //Debug.Log("Setting Pos Success4");
+
+
+
+            int xMidpoint = (int)CalculateMidPoint(startingPos.x, endingPos.x);
+            int yMidpoint = (int)CalculateMidPoint(startingPos.y, endingPos.y);
+
+            Debug.Log($"MidPointX: {xMidpoint}, MidPointY:{yMidpoint}");
+
+            raycastPoint.x = xMidpoint;
+            raycastPoint.y = yMidpoint;
+            raycastPoint.z = 0;
+
+            Debug.Log("Raycasting");
+            raycastTrue = true;
+            //TextureToWorldRaycast(xMidpoint, yMidpoint);
         }
         
+
         //Debug.Log($"First Pred: {classes[0]}");
     }
 
-    public void AddLabel()
-    {
-        GameObject virtualLabelInstance = GetPooledObject();
+    //public void AddLabel()
+    //{
+    //    GameObject virtualLabelInstance = GetPooledObject();
 
+    //}
+
+    public float InvertYPos(float pos)
+    {
+        return textureSize.y - pos;
     }
 
-    Vector3 testPos = new Vector3(0, 0, 0);
-    // Update is called once per frame
-    void Update()
+    public float CalculateMidPoint(float startingPos, float endingPos)
+    {
+        return startingPos + ((endingPos-startingPos)/2);
+    }
+
+    
+    public void TextureToWorldRaycast(Vector3 texturePos)
     {
 
-        Ray labelRay = RSCameraView.ScreenPointToRay(testPos);
+        RaycastHit hit;
+        Ray labelRay = RSCameraView.ScreenPointToRay(texturePos);
+
         Vector3 start = RSCameraView.transform.position;
+
         Vector3 end = labelRay.GetPoint(10000f);
         // OR
         // Vector3 end = start + ray.direction * 10000;
         Debug.DrawRay(start, end, Color.red);
+        if(Physics.Raycast(labelRay, out hit))
+        {
+            GameObject labelInstance = GetPooledObject();
+            if(labelInstance == null)
+            {
+                Debug.Log("NULL INSTANCE");
+            }
+            labelInstance.SetActive(true);
+            labelInstance.transform.position = hit.point;
+        }
+
+        //Debug.Log("Finished Drawing Ray");
+    }
+
+    bool raycastTrue = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (raycastTrue)
+        {
+            TextureToWorldRaycast(raycastPoint);
+            raycastTrue = false;
+        }
+        
+        
     }
 }
