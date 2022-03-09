cde76d961e2a32bcf126c63ccfc8b679775376f6
Juichilee <leejuic@oregonstate.edu>
2022-03-08 18:28:17 -0800

    vcadjustment

---
+++ Assets/MRIT/Scripts/VirtualCameraAdjustment.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualCameraAdjustment : MonoBehaviour
+{
+    public Camera RScamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
